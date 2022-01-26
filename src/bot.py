62e30292cf83f2d232280173f755ea7d0836f15a
Franreno <franreno4@gmail.com>
2022-01-26 10:49:01 -0300

    Increasing loop timer from 30 minutes to 90 minutes

--- src/bot.py
+++ src/bot.py
@@ -50,7 +50,7 @@ async def on_ready():
     # Check here the snake game last save in the database ###todo###
 
 
-@tasks.loop(minutes=30)
+@tasks.loop(minutes=90)
 async def SearchForTheList():
     flag, filename = SearchForFuvest()
     if(flag == True):
